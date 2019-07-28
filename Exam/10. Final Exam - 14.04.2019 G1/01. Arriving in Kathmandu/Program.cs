b7dbf212f236893c937d9cb549896a93c304379f
Neiko7 <neiko_grozev@abv.bg>
2019-07-28 09:25:14 +0300

    Add new exam!

---
+++ Exam/10. Final Exam - 14.04.2019 G1/01. Arriving in Kathmandu/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace ArrivingInKathmandu
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            string pattern = @"^(?<name>[A-Za-z\d!@#$?]+)=(?<length>\d+)<<(?<geohashcode>.*)$";
+            Regex regex = new Regex(pattern);
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Last note")
+                {
+                    break;
+                }
+
+                Match match = regex.Match(input);
+
+                if (!match.Success)
+                {
+                    PrintNotingFound(); 
+                    continue;
+                }
+
+                string namestr = match.Groups["name"].Value;
+                string name = string.Empty;
+
+                int length = int.Parse(match.Groups["length"].Value);
+                string geohashcode = match.Groups["geohashcode"].Value;
+
+                if (length != geohashcode.Length)
+                {
+                    PrintNotingFound();
+                    continue;
+                }
+
+                for (int i = 0; i < namestr.Length; i++)
+                {
+                    if (char.IsLetterOrDigit(namestr[i]))
+                    {
+                        name += namestr[i];
+                    }
+                }
+
+                Console.WriteLine($"Coordinates found! {name} -> {geohashcode}");
+            }
+        }
+
+        private static void PrintNotingFound()
+        {
+            Console.WriteLine("Nothing found!");
+        }
+    }
+}
---
+++ Exam/10. Final Exam - 14.04.2019 G1/02. On the Way to Annapurna/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace OnTheWayToAnnapurna
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "END")
+                {
+                    break;
+                }
+
+                string[] token = input
+                    .Split("->");
+
+                string command = token[0];
+                string store = token[1];
+
+
+                if (command == "Add")
+                {
+                    if (!dict.ContainsKey(store))
+                    {
+                        dict.Add(store, new List<string>());
+                    }
+
+                    string[] item = token[2]
+                        .Split(",");
+
+                    for (int i = 0; i < item.Length; i++)
+                    {
+                        dict[store].Add(item[i]);
+                    }
+                }
+                else if (command == "Remove")
+                {
+                    if (dict.ContainsKey(store))
+                    {
+                        dict.Remove(store);
+                    }
+                }
+            }
+
+            Console.WriteLine("Stores list:");
+
+            foreach (var store in dict.OrderByDescending(x => x.Value.Count).ThenByDescending(x => x.Key))
+            {
+                Console.WriteLine($"{store.Key}");
+
+                foreach (var item in store.Value)
+                {
+                    Console.WriteLine($"<<{item}>>");
+                }
+            }
+        }
+    }
+}
