18cddb47d425b5ae78104518a4a2df4cbc53252c
Neiko7 <neiko_grozev@abv.bg>
2019-07-29 10:01:16 +0300

    Add new exam!

---
+++ Exam/12. Final Exam - 06.04.2019/01. Dictionary/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace Dictionary
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            List<string> words = Console.ReadLine()
+                .Split("|")
+                .Select(x => x.Trim())
+                .ToList();
+
+            List<string> keys = Console.ReadLine()
+                .Split("|")
+                .Select(x => x.Trim())
+                .ToList();
+
+            string command = Console.ReadLine();
+
+            Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+            string pattern = $"(?<key>[A-Za-z]+): (?<value>.*)";
+            Regex regex = new Regex(pattern);
+
+            foreach (var word in words)
+            {
+                Match match = regex.Match(word);
+
+                if (match.Success)
+                {
+                    string key = match.Groups["key"].Value;
+                    string value = match.Groups["value"].Value;
+
+                    if (!dict.ContainsKey(key))
+                    {
+                        dict.Add(key, new List<string>());
+                    }
+
+                    dict[key].Add(value);
+                }
+            }
+
+            if (command == "List")
+            {
+                foreach (var item in dict.OrderBy(x => x.Key))
+                {
+                    Console.Write($"{item.Key} ");
+                }
+            }
+            else if (command == "End")
+            {
+                foreach (var key in keys)
+                {
+                    if (dict.ContainsKey(key))
+                    {
+                        Console.WriteLine($"{key}");
+
+                        foreach (var item in dict)
+                        {
+                            foreach (var word in item.Value.OrderByDescending(x => x.Length))
+                            {
+                                if (item.Key == key)
+                                {
+                                    Console.WriteLine($" -{word}");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Exam/12. Final Exam - 06.04.2019/02. Deciphering/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Deciphering
+{
+using System;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            string firstInput = Console.ReadLine();
+            string[] secondInput = Console.ReadLine()
+                .Split(" ");
+
+            string pattern = @"^[d-z{}\|#]+$";
+            Regex regex = new Regex(pattern);
+
+            Match match = regex.Match(firstInput);
+
+            StringBuilder decryptedWord = new StringBuilder();
+
+            if (match.Success)
+            {
+                for (int i = 0; i < firstInput.Length; i++)
+                {
+                    decryptedWord.Append((char)(firstInput[i] - 3));
+                }
+
+                decryptedWord.Replace(secondInput[0], secondInput[1]);
+
+                Console.WriteLine(string.Join("", decryptedWord));
+            }
+            else
+            {
+                Console.WriteLine("This is not the book you are looking for.");
+            }
+        }
+    }
+}
