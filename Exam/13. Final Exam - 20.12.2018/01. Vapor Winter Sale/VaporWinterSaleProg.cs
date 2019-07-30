47314f09d7a89213fbbe289b4ba0517ed831d1aa
Neiko7 <neiko_grozev@abv.bg>
2019-07-30 15:06:03 +0300

    Add new exam!

---
+++ Exam/13. Final Exam - 20.12.2018/01. Vapor Winter Sale/VaporWinterSaleProg.cs
@@ -0,0 +1,66 @@
+﻿namespace VaporWinterSale
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+    using System.Linq;
+
+    class VaporWinterSaleProg
+    {
+        static void Main()
+        {
+            Dictionary<string, double> games = new Dictionary<string, double>();
+            Dictionary<string, double> gamesWithDLC = new Dictionary<string, double>();
+
+            string[] input = Console.ReadLine()
+                .Split(", ");
+
+            string patternGames = @"(?<name>[A-Za-z\d ]+)-(?<price>\d+(?:\.\d+)?)";
+            string patternDLC = @"(?<name>[A-Za-z\d ]+):(?<DLC>.*)";
+
+            Regex regexGames = new Regex(patternGames);
+            Regex regexDLC = new Regex(patternDLC);
+
+            foreach (var item in input)
+            {
+                Match matchGame = regexGames.Match(item);
+                Match matchDLC = regexDLC.Match(item);
+
+                if (matchGame.Success)
+                {
+                    string name = matchGame.Groups["name"].Value;
+                    double price = double.Parse(matchGame.Groups["price"].Value);
+
+                    if (!games.ContainsKey(name))
+                    {
+                        games[name] = price;
+                    }
+                }
+                else if (matchDLC.Success)
+                {
+                    string name = matchDLC.Groups["name"].Value;
+                    string DLC = matchDLC.Groups["DLC"].Value;
+
+                    if (games.ContainsKey(name))
+                    {
+                        double value = games[name] * 1.2;
+                        gamesWithDLC.Add($"{name} - {DLC}", value);
+
+                        games.Remove(name);
+                    }
+                }
+            }
+            foreach (var item in gamesWithDLC.OrderBy(x => x.Value))
+            {
+                double value = item.Value * 0.5;
+                Console.WriteLine($"{item.Key} - {value:F2}");
+            }
+
+            foreach (var item in games.OrderByDescending(x => x.Value))
+            {
+                double value = item.Value * 0.8;
+                Console.WriteLine($"{item.Key} - {value:F2}");
+            }
+        }
+    }
+}
---
+++ Exam/13. Final Exam - 20.12.2018/02. Activation Keys/ActivationKeysProg.cs
@@ -0,0 +1,83 @@
+﻿namespace ActivationKeys
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class ActivationKeysProg
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine()
+                .Split("&", StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> keys = new List<string>();
+
+            foreach (var item in input)
+            {
+                StringBuilder key = new StringBuilder();
+                bool isValidChar = true;
+                bool isValidLength = false;
+
+                if (item.Length == 16)
+                {
+                    isValidLength = true;
+
+                    for (int i = 0; i < item.Length; i++)
+                    {
+                        if (!char.IsLetterOrDigit(item[i]))
+                        {
+                            isValidChar = false;
+                            break;
+                        }
+
+                        if (i % 4 == 0 && i != 0)
+                        {
+                            key.Append('-');
+                        }                       
+
+                        key.Append(item[i].ToString().ToUpper());
+                    }
+                }
+                else if (item.Length == 25)
+                {
+                    isValidLength = true;
+
+                    for (int i = 0; i < item.Length; i++)
+                    {
+                        if (!char.IsLetterOrDigit(item[i]))
+                        {
+                            isValidChar = false;
+                            break;
+                        }
+
+                        if (i % 5 == 0 && i != 0)
+                        {
+                            key.Append('-');
+                        }
+
+                        key.Append(item[i].ToString().ToUpper());
+                    }                    
+                }
+
+                if (isValidChar && isValidLength)
+                {
+                    for (int i = 0; i < key.Length; i++)
+                    {
+                        if (char.IsDigit(key[i]))
+                        {
+                            int digit = int.Parse(key[i].ToString());
+                            int currentDigit = 9 - digit;
+
+                            key[i] = char.Parse(currentDigit.ToString());
+                        }
+                    }
+
+                    keys.Add(key.ToString());
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", keys));
+        }
+    }
+}
