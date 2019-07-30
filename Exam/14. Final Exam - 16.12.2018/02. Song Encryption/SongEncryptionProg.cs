9c95ec2c3d984e2733f28cde692491e98306cd4a
Neiko7 <neiko_grozev@abv.bg>
2019-07-30 18:02:59 +0300

    Add new exam!

---
+++ Exam/14. Final Exam - 16.12.2018/02. Song Encryption/SongEncryptionProg.cs
@@ -0,0 +1,107 @@
+﻿namespace SongEncryption
+{
+    using System;
+
+    class SongEncryptionProg
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "end")
+                {
+                    break;
+                }
+
+                string[] arr = input
+                    .Split(":");
+
+                string artist = arr[0];
+                string song = arr[1];
+
+                bool isArtist = true;
+                bool isSong = true;
+
+                if (artist[0] >= 'A' && artist[0] <= 'Z')
+                {
+                    for (int i = 1; i < artist.Length; i++)
+                    {
+                        if (!(artist[i] >= 'a' && artist[i] <= 'z' || artist[i] == ' ' || artist[i] == '\''))
+                        {
+                            isArtist = false;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    isArtist = false;
+                }
+
+                for (int i = 0; i < song.Length; i++)
+                {
+                    if (!(song[i] >= 'A' && song[i] <= 'Z' || song[i] == ' '))
+                    {
+                        isSong = false;
+                        break;
+                    }
+                }
+
+                if (isArtist && isSong)
+                {
+                    string decrypted = string.Empty;
+                    int key = artist.Length;
+
+                    for (int i = 0; i < input.Length; i++)
+                    {
+                        if (input[i] == ' ')
+                        {
+                            decrypted += ' ';
+                        }
+                        else if (input[i] == '\'')
+                        {
+                            decrypted += '\'';
+                        }
+                        else if (input[i] >= 'A' && input[i] <= 'Z')
+                        {
+                            if ((char)(input[i] + key) > 'Z')
+                            {
+                                int temp = key - ('Z' - input[i]);
+                                decrypted += (char)(64 + temp);
+                            }
+                            else
+                            {
+                                decrypted += (char)(input[i] + key);
+                            }
+                        }
+                        else if (input[i] >= 'a' && input[i] <= 'z')
+                        {
+                            if ((char)(input[i] + key) > 'z')
+                            {
+                                int temp = key - ('z' - input[i]);
+                                decrypted += (char)(96 + temp);
+                            }
+                            else
+                            {
+                                decrypted += (char)(input[i] + key);
+                            }
+                        }
+                        else if (input[i] == ':')
+                        {
+                            decrypted += '@';
+                        }
+
+                    }
+
+                    Console.WriteLine($"Successful encryption: {decrypted}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+            }
+        }
+    }
+}
